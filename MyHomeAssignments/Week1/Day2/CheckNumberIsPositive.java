665a851104529b91fc5130cae97d7ce223a15e7c
contactramasami <115720238+contactramasami@users.noreply.github.com>
2024-09-17 21:56:15 +0530

    Create CheckNumberIsPositive.java

---
+++ MyHomeAssignments/Week1/Day2/CheckNumberIsPositive.java
@@ -0,0 +1,22 @@
+package Assignment_Week1day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) 
+	{
+		int num=40;
+		if(num>0)
+		{
+			System.out.println(num + " is Positive Number");
+		}
+		else
+		{
+			System.out.println(num + " is Negative Number");
+
+			
+		}
+
+
+	}
+
+}
