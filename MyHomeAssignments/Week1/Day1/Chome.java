e2ec8c8b55f0a6cc96df89db8090c2e58f4151d6
contactramasami <115720238+contactramasami@users.noreply.github.com>
2024-09-17 17:11:58 +0530

    Create Chome.java

---
+++ MyHomeAssignments/Week1/Day1/Chome.java
@@ -0,0 +1,23 @@
+package Assignment_Week1Day1;
+
+public class Chrome 
+{
+
+	public static void main(String[] args) 
+	{
+		
+					float browserVersion = 90.4f;
+					String browserName = "firefox"; 
+					boolean isVisible = true; 
+					int releaseYear = 2011; 
+					char browserLogo = 'f';
+					System.out.println("The Version of the Browser is : "+browserVersion);
+					System.out.println("The Browser Used is : "+browserName);
+					System.out.println("Is the Browser Visible ? :"+isVisible);
+					System.out.println("Which Year was it released ? :"+releaseYear);
+					System.out.println("Logo of the Browser : "+browserLogo);
+		
+
+	}
+
+}
